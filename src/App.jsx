a721c3580c28fb5b03659188b81947bebdcccbd6
Maksim <maks1733@gmail.com>
2024-01-15 21:47:30 +0400

    Настроить проект

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components'
+
+const Div = styled.div`
+  text-align: center;
+`;
+
+function App() {
+
+
+  return (
+    <Div>
+      <div>123</div>
+      <i className="fa fa-camera-retro fa-lg"></i>
+    </Div>
+  )
+}
+
+export default App
